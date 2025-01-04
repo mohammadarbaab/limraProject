5e0e73b67c27cca56d559f88504a4fe81cd331e4
mohammadarbaab <kazmiwrite@gmail.com>
2025-01-04 23:16:14 +0530

    filter doctors

--- lch-project/src/Pages/Doctors.jsx
+++ lch-project/src/Pages/Doctors.jsx
@@ -7,7 +7,7 @@ function Doctors() {
   const { doctors } = useContext(AppContext);
   const navigate = useNavigate();
   const [filterDoc, setFilterDoc] = useState([]);
-
+  const [showFilter, setShowFilter] = useState(false);
   const applyFilter = () => {
     if (speciality) {
       setFilterDoc(doctors.filter((doc) => doc.speciality === speciality));
@@ -24,7 +24,15 @@ function Doctors() {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <button
+          onClick={() => setShowFilter((prev) => !prev)}
+          className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${
+            showFilter ? "bg-primary text-white" : ""
+          }`}
+        >
+          Filters
+        </button>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex':'hidden sm:flex'}`}>
           <p
             onClick={() =>
               speciality === "General physician"
