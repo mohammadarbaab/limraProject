3bf14106e3322c9f1c8b6728986a24584236dffe
mohammadarbaab <kazmiwrite@gmail.com>
2025-01-14 20:18:15 +0530

    admin doctor add form

--- lch-project/admin/src/components/Sidebar.jsx
+++ lch-project/admin/src/components/Sidebar.jsx
@@ -6,25 +6,53 @@ import { assets } from "../assets/assets";
 function Sidebar() {
   const { aToken } = useContext(AdminContext);
   return (
-    <div>
+    <div className="min-h-screen bg-white border-r">
       {aToken && (
-        <ul>
-          <NavLink to={'/admin-dashboard'}>
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            to={"/admin-dashboard"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+          >
             <img src={assets.home_icon} alt="" />
             <p>Dashboard</p>
           </NavLink>
 
-          <NavLink to={'/all-appointments'}>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/all-appointments"}
+          >
             <img src={assets.appointment_icon} alt="" />
             <p>Appointments</p>
           </NavLink>
 
-          <NavLink to={'/add-doctor'}>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/add-doctor"}
+          >
             <img src={assets.add_icon} alt="" />
             <p>Add Doctor</p>
           </NavLink>
 
-          <NavLink to={'/doctor-list'}>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/doctor-list"}
+          >
             <img src={assets.people_icon} alt="" />
             <p>Doctor List</p>
           </NavLink>
